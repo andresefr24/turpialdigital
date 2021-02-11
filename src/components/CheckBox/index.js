efcb7b8b8193e322d802548fe05caa4d10291e4b
Oriana <oriabreu.v@gmail.com>
2021-02-11 13:50:23 +0100

    add MUI disabled button
    config button styles and functionality

--- src/components/CheckBox/index.js
+++ src/components/CheckBox/index.js
@@ -2,24 +2,15 @@ import React, { useState } from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
 import useStyles from "./styles";
 
-export default function MyCheckBox() {
-  const [checked, setChecked] = useState(false);
-
+export default function MyCheckBox(props) {
   const {checkbox} = useStyles();
 
-  const handleChange = (event) => {
-    setChecked(event.target.checked);
-  };
-
   return (
-    <div>
       <Checkbox
-        checked={checked}
-        onChange={handleChange}
         inputProps={{ 'aria-label': 'primary checkbox' }}
         className={checkbox}
         color='#fdcf08'
+        {...props}
       />
-    </div>
   );
 }
